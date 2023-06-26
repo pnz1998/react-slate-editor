dd95d17910adf84cdd9a9ce39a5e84078fe0b388
nanzhe21 <parknc@aliyun.com>
2023-06-26 22:01:51 +0800

    change

--- src/components/Headerbar.tsx
+++ src/components/Headerbar.tsx
@@ -1,35 +1,71 @@
-import { Box, Input } from "@mui/material";
+import { Box, TextField, styled } from "@mui/material";
 import { FC } from "react";
 import { Search } from "./Icons";
 
 const Headerbar: FC = () => {
+  const HeaderSearchInput = styled(TextField)(({ theme }) => ({
+    '& .MuiInputBase-root': {
+      borderRadius: "0 4px 4px 0",
+    },
+    '& .MuiOutlinedInput-root': {
+      height: "40px",
+    },
+    '& .MuiOutlinedInput-notchedOutline': {
+      display: "none"
+    },
+    '& .MuiInputBase-input':  {
+      padding: 0,
+      background: "#fff",
+      height: "40px",
+      width: "400px",
+      borderRadius: "0 4px 4px 0",
+      '&:hover':  {
+        border: "none"
+      },
+    }
+  }));
   return (
     <Box
       sx={{
         width: "100%",
         height: "100%",
         background: "#1976d2",
+        display: "flex"
       }}
     >
-      <Box 
-        component="form"
+      <Box
         sx={{
+          width: "90%",
+          height: "100%",
+          lineHeight: "60px",
+          display: "flex",
+          alignItems: "center"
+        }}
+      >
+        <Box
+          sx={{
+            width: "40px",
+            height: '40px',
+            background: "#fff",
+            textAlign: "center",
+            borderRadius: "4px 0 0 4px",
+            lineHeight: "52px"
+          }}
+        >
+          <Search sx={{ color: "#1976d2", fontSize: "26px" }}/>
+        </Box>
+        <HeaderSearchInput 
+          placeholder="查询..."
+        />
+      </Box>
+      <Box
+        sx={{
+          flex: 1,
           height: "100%",
           lineHeight: "60px"
         }}
       >
-        <Input 
-          value="search" 
-          startAdornment={<Search sx={{ color: "#fff" }}/>}
-          sx={{
-            '& .MuiInput-input': {
-              background: "#fff",
-              '&::before': {
-                border: "none"
-              }
-            },
-          }}
-        />
+        头像
       </Box>
     </Box>
   )
